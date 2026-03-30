ec5d179929861f332772a41e97c2392bfa7da5af
suryanshtomar04 <st7639@srmist.edu.in>
2026-03-30 21:57:52 +0530

    API CONNECTION LAYER

---
+++ client/src/services/api.js
@@ -0,0 +1,63 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'http://localhost:5000/api';
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Add a request interceptor to include the auth token if available
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export const loginUser = async (credentials) => {
+  const response = await api.post('/users/login', credentials);
+  return response.data;
+};
+
+export const registerUser = async (userData) => {
+  const response = await api.post('/users/register', userData);
+  return response.data;
+};
+
+export const getAllParking = async () => {
+  const response = await api.get('/parking');
+  return response.data;
+};
+
+export const getNearbyParking = async (lat, lng) => {
+  const response = await api.get('/parking/nearby', {
+    params: { lat, lng },
+  });
+  return response.data;
+};
+
+export const getParkingById = async (id) => {
+  const response = await api.get(`/parking/${id}`);
+  return response.data;
+};
+
+export const createBooking = async (bookingData) => {
+  const response = await api.post('/bookings', bookingData);
+  return response.data;
+};
+
+export const getUserBookings = async () => {
+  const response = await api.get('/bookings');
+  return response.data;
+};
+
+export default api;
